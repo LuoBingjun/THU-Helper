aab8a5ae9f61f38446fac12cbdd90f0b7e20dba3
Bingjun Luo <luobingjun@gmail.com>
2020-06-18 11:57:56 +0800

    support for backend

--- app/src/main/java/com/example/thu_helper/data/LoginDataSource.java
+++ app/src/main/java/com/example/thu_helper/data/LoginDataSource.java
@@ -2,15 +2,23 @@ package com.example.thu_helper.data;
 
 import androidx.annotation.Nullable;
 
+import com.example.thu_helper.R;
 import com.example.thu_helper.data.model.LoggedInUser;
+import com.example.thu_helper.utils.Global;
+
+import org.json.JSONObject;
 
 import java.io.IOException;
 
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
+
+import static android.provider.Settings.System.getString;
 
 /**
  * Class that handles authentication w/ login credentials and retrieves user information.
@@ -21,17 +29,20 @@ public class LoginDataSource {
         try {
             // TODO: handle loggedInUser authentication
             OkHttpClient client = new OkHttpClient();
+//            RequestBody body = new FormBody.Builder().add("username", username).add("password", password).build();
             Request request = new Request.Builder()
-                    .url("https://www.baidu.com")
+                    .url(Global.url_prefix + "/user/login?ID=" + username + "&password=" + password)
                     .build();
             Call call = client.newCall(request);
             Response response = call.execute();
             if (response.isSuccessful()) {
-                LoggedInUser fakeUser =
-                        new LoggedInUser(
-                                java.util.UUID.randomUUID().toString(),
-                                "Jane Doe");
-                return new Result.Success<>(fakeUser);
+                JSONObject res = new JSONObject(response.body().string());
+                String token = res.get("token").toString();
+                LoggedInUser user = new LoggedInUser(
+                        username,
+                        "张三",
+                        token);
+                return new Result.Success<>(user);
             }
             return new Result.Error(new IOException("Login Error"));
         } catch (Exception e) {
