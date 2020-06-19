6659cc9ab3f600dcb12dd5132986ab37c06029af
Bingjun Luo <luobingjun@gmail.com>
2020-06-19 22:59:45 +0800

    modify login order

--- app/src/main/java/com/example/thu_helper/MainActivity.java
+++ app/src/main/java/com/example/thu_helper/MainActivity.java
@@ -42,12 +42,7 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
 //        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
-
-        if (!loginRepository.isLoggedIn()){
-            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-//            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(intent);
-        }
+        
     }
 
 }
